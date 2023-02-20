68469ec42161181f2077442c1ee76332c183c785
ReptiloidAnunak <120068158+ReptiloidAnunak@users.noreply.github.com>
2023-02-20 01:44:49 -0300

    Delete coursework3_inst directory

--- coursework3_inst/logger_api.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger_api = logging.getLogger("logger_api")
-file_handler = logging.FileHandler("logs/api.logs")
-formatter_api = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-file_handler.setFormatter(formatter_api)
-logger_api.addHandler(file_handler)
-logger_api.setLevel(logging.INFO)
